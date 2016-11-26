94903ec4011af243de22eceaec23deab22c3fabe
Marko Grumic <marko.grumic@mail.ru>
2016-11-26 13:26:39 +0100

    Changed infinite loop in boot.s where infinite loop cals hlt and cli instructions

--- kernel/boot.s
+++ kernel/boot.s
@@ -83,7 +83,7 @@ _start:
 	;    Since they are disabled, this will lock up the computer.
 	; 3) Jump to the hlt instruction if it ever wakes up due to a
 	;    non-maskable interrupt occurring or due to system management mode.
-	;cli
-.hang:	;hlt
+	cli
+.hang:	hlt
 	jmp .hang
 .end:
